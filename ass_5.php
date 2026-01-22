3419256f6a07aa7b97ad78a664326e4a9b32e471
nahida24iot07gecv-ai <nahida24iot07.gecv@gmail.com>
2026-01-22 13:51:07 +0530

    Add files via upload

---
+++ ass_5.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+$length = 20;
+$width = 10;
+$area = $length * $width;
+
+echo "Length: $length, Width: $width<br>";
+echo "Area of the Rectangle: $area";
+?> 
+</body>
+</html>
