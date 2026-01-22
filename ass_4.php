c7f516e134790e31d53598d68b61e8c13df46605
nahida24iot07gecv-ai <nahida24iot07.gecv@gmail.com>
2026-01-22 13:50:37 +0530

    Add files via upload

---
+++ ass_4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$base = 10;
+$height = 15;
+$area = 0.5 * $base * $height;
+
+echo "Base: $base, Height: $height<br>";
+echo "Area of the Triangle: $area";
+?>
+</body>
+</html>
